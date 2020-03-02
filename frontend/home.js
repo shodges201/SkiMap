a354000e3fd59debf67d37c525d73bd8c273b863
Scott Hodges <shodges201@gmail.com>
2020-03-02 18:41:54 -0500

    reorganized server routes so both the binary string for the map and a json response can be obtained from the server without repeating code. Also refactored front end HTML and js to match the changes on the backend

--- frontend/home.js
+++ frontend/home.js
@@ -1,22 +1,16 @@
 $(document).ready(function(){
-    console.log("ready");
     $("#trail-info-btn").click(function(){
-        console.log("clicked");
         $("#open-list").empty();
         $("#closed-list").empty();
-        jQuery.get('/status/abasin', function(data, status){
-            console.log(status);
+        jQuery.get('/abasin', function(data, status){
             if(status === "success"){
-                console.log("here");
-                const open = data.open;
-                const closed = data.closed;
-                for(let i = 0; i < open.length; i++){
-                    $("#open-list").append($("<li>").text(open[i]));
+                const {openTrails, closedTrails} = data;
+                for(let i = 0; i < openTrails.length; i++){
+                    $("#open-list").append($("<li>").text(openTrails[i]));
                 }
-                for(let i = 0; i < closed.length; i++){
-                    $("#closed-list").append($("<li>").text(closed[i]));
+                for(let i = 0; i < closedTrails.length; i++){
+                    $("#closed-list").append($("<li>").text(closedTrails[i]));
                 }
-                console.log(open.length + closed.length)
             }
         })
     })
--- server.js
+++ server.js
@@ -11,18 +11,22 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend/home.html'));
 })
 
-app.get('/status/abasin', (req, res) => {
-    request('https://www.arapahoebasin.com/snow-conditions/terrain/', function (error, response, html) {
-        if (!error && response.statusCode == 200) {
-            const $ = cheerio.load(html);
-            const openTrails = $('.status_open').parent();
-            const closedTrails = $('.status_closed').parent();
+app.get('/abasin', (req, res) => {
+    const notIncluded = ['Open', 'Closed', 'From 5:00 AM until 8:00 AM', 'During Ski Area Operational Hours', 'From Ski Area Closing until 5:00 AM',
+        "Lower Mountain Blues", "Lower Mountain Greens", "Terrain Parks", "East Wall", "East Wall Hiking", "Lower Lenawee Zone", "Upper Lenawee Zone", "West Wall Zone", "North Glade Zone", "Pallavinci", "Slalom Slope/Grizzly Road", "Standard/Exhibition Zone", "Steep Gullies & Hike Back Terrain", "East Zuma", "Hike Back Terrain", "MGD Zone", "Montezuma Bowl Blues", "West Zuma", "Beavers Intermediate Terrain", "Lower Beavers Zone", "Upper Beavers Zone", "Molly Hogan", "Molly's Magic Carpet", "Pika Place Carpet", "Pika Place"];
+    getAbasinInfo((info) => {
+        console.log(info);
+        res.json(info);
+    });
+})
 
-            let map = new Map();
-            const orderedList = ["Moose Hollow", "North Fork", "Exhibition", "Sundance", "Wrangler", "Tree Chutes 1-8", "North Y Chute", "North Y Chute",
+app.get('/status/abasin', (req, res) => {
+    getAbasinInfo(({ openTrails, closedTrails }) => {
+        let map = new Map();
+        const orderedList = ["Moose Hollow", "North Fork", "Exhibition", "Sundance", "Wrangler", "Tree Chutes 1-8", "North Y Chute", "North Y Chute",
             "South Y Chute", "Corner Chute", "Corner Chute", "Willy's Wide", "North Pole", "Falcon", "Humbug", "Zuma Lift", "Ptarmigan", "Loafer", "Peaceful Valley",
             "The Last Waltz", "Dreamcatcher", "Bighorn", "Bighorn", "Glockenspiel Glade", "Pioneer Willy", "Drummond", "Bald Spot", "Janitors Only", "Christmas Trees",
-            "Pali Wog", "Rock Garden", "Turbo", "West Turbo", "Timber Glades", "East Avenue", "Pali Main Street", "Pallavicini", "Pallavicini", "East Woods", 
+            "Pali Wog", "Rock Garden", "Turbo", "West Turbo", "Timber Glades", "East Avenue", "Pali Main Street", "Pallavicini", "Pallavicini", "East Woods",
             "Face Shot Gully", "Thick & Thin", "Bailey Bros.", "Faculty Club", "Jaeger", "Hauk", "Castor", "The Spine", "Pali Face", "1st Alley - David's Run", "2nd Alley",
             "3rd Alley", "4th Alley (West Alley)", "Gauthier", "SG 1", "SG 2", "SG 3", "SG 4", "SG 5", "The Cellar", "Jetta", "Alex", "Digger", "Beaver Bowl", "Marmot", "Davis",
             "Wildcat", "West Wall", "Jamie's Face", "Half Moon Glades", "Cabin Glades", "East Gully", "East Wall - Below the Traverse", "Land of the Giants", "Dragon", "West Gully",
@@ -31,27 +35,46 @@ app.get('/status/abasin', (req, res) => {
             "Challenger Zone", "Lynx Lane", "TB Glades", "Upper Chisholm Trail", "High Noon", "The Gulch", "Ramrod", "Weasel Way", "Lower Chisholm Trail", "Lenawee Mountain Lift",
             "Norway Face", "Norway Mountain Run", "Beavers"];
 
-            listOfTrails(openTrails, 1, map);
-            listOfTrails(closedTrails, 0, map);
-\
-            let binaryArray = [];
-            // a character at the beginning of the data to signify that the rest of the data is the ordered binary string
-            binaryArray.push(String.fromCharCode(2));
-            for(let i = 0; i < orderedList.length; i++){
-                let bit = map.get(orderedList[i]);
-                if(bit === undefined){
-                    console.log(`${orderedList[i]} cannot be found in scraped data and returned a value of ${bit}`);
-                }
-                binaryArray.push(bit);
+        mapOfTrails(openTrails, 1, map);
+        mapOfTrails(closedTrails, 0, map);
+
+        console.log(map);
+
+        let binaryArray = [];
+        // a character at the beginning of the data to signify that the rest of the data is the ordered binary string
+        binaryArray.push(String.fromCharCode(2));
+        for (let i = 0; i < orderedList.length; i++) {
+            let bit = map.get(orderedList[i]);
+            if (bit === undefined) {
+                console.log(`${orderedList[i]} cannot be found in scraped data and returned a value of ${bit}`);
             }
-            return res.send(binaryArray.join(""));
+            binaryArray.push(bit);
         }
-    });
+        return res.send(binaryArray.join(""));
+    })
 })
 
-//takes a list of open or closed trails and a value to set these trails to in the map that is also passed in
-//valueInMap is either 1 or 0
-function listOfTrails(list, valueInMap, map) {
+function getAbasinInfo(cb) {
+    request('https://www.arapahoebasin.com/snow-conditions/terrain/', function (error, response, html) {
+        if (!error && response.statusCode == 200) {
+            const $ = cheerio.load(html);
+            const openTrails = $('.status_open').parent();
+            const closedTrails = $('.status_closed').parent();
+
+            const openTrailsList = formatAbasinInfo(openTrails);
+            const closedTrailsList = formatAbasinInfo(closedTrails);
+
+            const trailInfo = {
+                openTrails: openTrailsList,
+                closedTrails: closedTrailsList
+            }
+            return cb(trailInfo);
+        }
+        throw Error("could not access a basin info at this time");
+    })
+}
+
+function formatAbasinInfo(list) {
     trailsList = [];
     const notIncluded = ['Open', 'Closed', 'From 5:00 AM until 8:00 AM', 'During Ski Area Operational Hours', 'From Ski Area Closing until 5:00 AM',
         "Lower Mountain Blues", "Lower Mountain Greens", "Terrain Parks", "East Wall", "East Wall Hiking", "Lower Lenawee Zone", "Upper Lenawee Zone", "West Wall Zone", "North Glade Zone", "Pallavinci", "Slalom Slope/Grizzly Road", "Standard/Exhibition Zone", "Steep Gullies & Hike Back Terrain", "East Zuma", "Hike Back Terrain", "MGD Zone", "Montezuma Bowl Blues", "West Zuma", "Beavers Intermediate Terrain", "Lower Beavers Zone", "Upper Beavers Zone", "Molly Hogan", "Molly's Magic Carpet", "Pika Place Carpet", "Pika Place"]
@@ -60,51 +83,23 @@ function listOfTrails(list, valueInMap, map) {
         for (let j = 0; j < trail.children.length; j++) {
             const child = trail.children[j];
             if (child.type === "text") {
-                if (child.data.trim() && !notIncluded.includes(child.data.trim()) && child.data.trim() !== trailsList[trailsList.length - 1]) {
-                    map.set(child.data.trim(), valueInMap);
+                const trailName = child.data.trim();
+                if (trailName && !notIncluded.includes(trailName) && trailName !== trailsList[trailsList.length - 1]) {
+                    trailsList.push(trailName);
                 }
             }
         }
     }
-    return;
+    return trailsList;
 }
 
-
-
-app.get('/status/bromley', (req, res) => {
-    let openList;
-    let closedList;
-    request('https://www.bromley.com/the-mountain/snow-report/?gclid=CjwKCAiAuqHwBRAQEiwAD-zr3Zsnjp-8ANKUpyu_0CnnXRHzdgs6XwJDWK0WnMUM9SxqHczh3CrdjhoCTNwQAvD_BwE', function (error, response, html) {
-        if (!error && response.statusCode == 200) {
-            const $ = cheerio.load(html);
-            
-            const openTrails = $('.lift-status-section').children(".lift-status-info").children('.lift-status-icon').children(".icon_snowreport_open").parent().parent().children(".lift-name");
-            const closedTrails = $('.lift-status-section').children().children('.icon_snowreport_closed');
-
-            console.log(openTrails[0]);
-            //console.log(closedTrails);
-
-            /*openList = listOfTrails(openTrails);
-            closedList = listOfTrails(closedTrails);
-
-            console.log(closedList.length + openList.length);
-
-
-            res.send({
-                "open": openList,
-                "closed": closedList
-            })*/
-
-            // res.send({
-            //     "open": openTrails
-            // })
-        }
-    })
-})
-
-app.get('/bromley', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/bromley/bromley.html'));
-})
+//takes a list of open or closed trail names and a value to set these trails to in the map that is also passed in
+//valueInMap is either 1 or 0
+function mapOfTrails(list, valueInMap, map) {
+    for (let i = 0; i < list.length; i++) {
+        map.set(list[i], valueInMap);
+    }
+}
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
